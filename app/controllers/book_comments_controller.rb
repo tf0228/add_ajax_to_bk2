67f0169c640605404a8dc0eff823a0508a54a9c6
vagrant <vagrant@localhost.localdomain>
2020-08-19 07:27:49 +0000

    [Finish] ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,22 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    respond_to do |format|
+      format.html { redirect_to book_path(@book) }
+      format.js
+    end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    respond_to do |format|
+      format.html { redirect_to book_path(params[:book_id]) }
+      format.js { @book = Book.find(params[:book_id]) }
+    end
   end
 
   private
